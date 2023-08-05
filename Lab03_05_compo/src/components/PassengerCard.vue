4a3a00b2f85bcf7cf654404d8e880c1716fcb393
nureman <nureman_s@cmu.ac.th>
2023-08-05 13:11:10 +0700

    add Event card

---
+++ Lab03_05_compo/src/components/PassengerCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="passenger-card">
+    <h3>{{ passenger.first_name }} {{ passenger.last_name }}</h3>
+    <p>{{ passenger.email }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { props } from 'vue'
+import type { PassengerInfo } from '@/type'
+
+// Define props for the component
+const props = defineProps({
+  passenger: {
+    type: Object as () => PassengerInfo,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.passenger-card {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 10px 0;
+}
+</style>
