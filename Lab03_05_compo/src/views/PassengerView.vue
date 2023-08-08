0cb2c416e5af52d94b3210f85fd8dd94a5da15fa
nureman <nureman_s@cmu.ac.th>
2023-08-08 15:20:19 +0700

    add next prev link

--- Lab03_05_compo/src/views/PassengerView.vue
+++ Lab03_05_compo/src/views/PassengerView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import type { PassengerInfo } from '@/type'
-import { ref, watchEffect } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
 import type { Ref } from 'vue'
 import PassengerService from '@/services/PassengerService'
 import PassengerCard from '@/components/PassengerCard.vue'
+import type { Axios, AxiosResponse } from 'axios'
 const passengers: Ref<Array<PassengerInfo>> = ref([])
-
+// const eventsPerPage = ref(2)
+const totalEvent = ref<number>(0)
 const props = defineProps({
   page: {
     type: Number,
@@ -14,9 +16,14 @@ const props = defineProps({
 })
 
 watchEffect(() => {
-  PassengerService.getEvent(5, props.page).then((response) => {
+  PassengerService.getEvent(5, props.page).then((response: AxiosResponse<PassengerInfo[]>) => {
     passengers.value = response.data
+    totalEvent.value = response.headers['x-total-count']
   })
+})
+const hasNextPages = computed(() => {
+  const totalPages = Math.ceil(totalEvent.value / 5)
+  return props.page.valueOf() < totalPages
 })
 </script>
 
@@ -27,5 +34,23 @@ watchEffect(() => {
       :key="passenger.id"
       :passenger="passenger"
     ></PassengerCard>
+    <div class="pagination">
+      <RouterLink
+        :to="{ name: 'passenger', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        id="page-prev"
+      >
+        Prev page
+      </RouterLink>
+      <RouterLink
+        :to="{ name: 'passenger', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPages"
+        id="page-next"
+      >
+        Next page
+      </RouterLink>
+    </div>
   </main>
 </template>
